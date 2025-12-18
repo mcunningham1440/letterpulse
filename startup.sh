388de2497f32408a451538ef4360fb2421292113
mcunningham1440 <mcunningham1440@gmail.com>
2025-12-17 21:42:36 -0600

    Lengthened Gunicorn timeout

--- startup.sh
+++ startup.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 manage.py collectstatic && gunicorn --workers 2 beehiiv_analytics.wsgi
+python3 manage.py collectstatic 
+
+exec gunicorn beehiiv_analytics.wsgi:application \
+  --bind 0.0.0.0:${PORT:-8000} \
+  --workers 1 \
+  --threads 4 \
+  --timeout 120 \
+  --graceful-timeout 30
