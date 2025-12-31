956ca18720308231b052071286d313b43c150dd4
mcunningham1440 <mcunningham1440@gmail.com>
2025-12-30 23:32:38 -0600

    Attempted new fix to dependencies issue

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ python3 -m pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
 
-exec gunicorn beehiiv_analytics.wsgi:application \
+exec python3 -m gunicorn beehiiv_analytics.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 1 \
   --threads 4 \
