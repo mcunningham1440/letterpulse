c4cee38c42b4c918e2c77680d928a11192354699
mcunningham1440 <mcunningham1440@gmail.com>
2025-12-31 11:02:46 -0600

    4th attempted fix to dependencies issue

--- startup.sh
+++ startup.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+set -eux
 
-set -e
+pwd
+ls -la
 
-python3 -m pip install -U pip
-python3 -m pip install -r requirements.txt
+# Create venv fresh at runtime (so we know it exists)
+python3 -m venv /tmp/venv
+. /tmp/venv/bin/activate
 
-python3 manage.py collectstatic --noinput
+python -V
+python -m pip -V
 
-exec python3 -m gunicorn beehiiv_analytics.wsgi:application \
+# Install deps and FAIL if it doesn't work
+python -m pip install -U pip
+python -m pip install -r requirements.txt
+
+# Prove imports work (this will fail loudly if deps aren't there)
+python -c "import django, gunicorn; print('django', django.get_version())"
+
+python manage.py collectstatic --noinput
+
+exec python -m gunicorn beehiiv_analytics.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 1 \
   --threads 4 \
