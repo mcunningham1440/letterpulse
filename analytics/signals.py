29d0aa3b30058cd0f31223d1a76ea5cdbfd46c63
mcunningham1440 <mcunningham1440@gmail.com>
2026-03-07 17:19:17 -0600

    Added first wins to signup notification email and switched to HTML format

--- analytics/signals.py
+++ analytics/signals.py
@@ -6,7 +6,7 @@ import logging
 import threading
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
@@ -75,26 +75,60 @@ def _send_welcome_email(user):
         try:
             user.refresh_from_db()
             first_name = user.first_name.strip()
-            greeting = f"Hi {first_name},\n" if first_name else "Hi,\n"
-            body = (
-                f"{greeting}\n"
-                "Thanks for signing up to try out LetterPulse!\n\n"
+            greeting = f"Hi {first_name}," if first_name else "Hi,"
+            html_body = (
+                f"{greeting}<br><br>"
+                "Thanks for signing up to try out LetterPulse!<br><br>"
+
+                "<b>Some quick wins to get value from your first session:</b><br><br>"
+
+                "1. Once you've entered your Beehiiv credentials and downloaded your posts, "
+                "<b>process a few of your recent issues</b> with at least 2-3 recurring sections in your newsletter. "
+                "<i>Make sure to use real sections! The app won't work properly if given made-up ones like 'just testing'.</i><br>"
+                "2. <b>Check out the section performance chart</b> in the Insights tab to see how each section is performing over time.<br>"
+                "3. <b>Run &quot;Get Insights&quot;</b> to see what kind of content in those sections performs best.<br><br>"
+
                 "The app has informational tooltips to walk you through getting started, "
-                "but please reach out if anything is unclear--I'm always happy to help.\n\n"
+                "but please reach out if anything is unclear\u2014I'm always happy to help.<br><br>"
+
+                "I'll reach back out in a few days once you've had a chance to try it out, "
+                "but if you have any questions or comments in the meantime, please let me know! "
+                "<b>You're one of the first people to test out LetterPulse so your feedback will be very helpful.</b><br><br>"
+                "<div>Best,<br><br>"
+                "Michael Cunningham<br>"
+                "Founder, LetterPulse.com</div>"
+            )
+            plain_body = (
+                f"{greeting}\n\n"
+                "Thanks for signing up to try out LetterPulse!\n\n"
+
+                "Some quick wins to get value from your first session:\n\n"
+
+                "1. Once you've entered your Beehiiv credentials and downloaded your posts, "
+                "process a few of your recent issues with at least 2-3 recurring sections in your newsletter. "
+                "Make sure to use real sections! The app won't work properly if given made-up ones like 'just testing'.\n"
+                "2. Check out the section performance chart in the Insights tab to see how each section is performing over time.\n"
+                "3. Run \"Get Insights\" to see what kind of content in those sections performs best.\n\n"
+
+                "The app has informational tooltips to walk you through getting started, "
+                "but please reach out if anything is unclear\u2014I'm always happy to help.\n\n"
+
                 "I'll reach back out in a few days once you've had a chance to try it out, "
                 "but if you have any questions or comments in the meantime, please let me know! "
                 "You're one of the first people to test out LetterPulse so your feedback will be very helpful.\n\n"
                 "Best,\n\n"
+
                 "Michael Cunningham\n"
                 "Founder, LetterPulse.com"
             )
-            send_mail(
+            msg = EmailMultiAlternatives(
                 subject="Welcome to LetterPulse!",
-                message=body,
+                body=plain_body,
                 from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[user.email],
-                fail_silently=True,
+                to=[user.email],
             )
+            msg.attach_alternative(html_body, "text/html")
+            msg.send(fail_silently=True)
         except Exception:
             logger.exception("Failed to send welcome email to %s", user.email)
 
